9d1c4d38f9117ec626cf2a991ff42679b1601eb0
Souvik Goswami <soufrk@gmail.com>
2018-01-15 13:05:25 +0530

    Modified pom, added new packages.

---
+++ implementations/src/main/java/com/my/project/implementations/statefulobjects/UserInputStateChange.java
@@ -0,0 +1,10 @@
+package com.my.project.implementations.statefulobjects;
+
+public class UserInputStateChange {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
---
+++ implementations/src/main/java/com/my/project/implementations/threadsandconcurrency/ThreadsInChain.java
@@ -0,0 +1,10 @@
+package com.my.project.implementations.threadsandconcurrency;
+
+public class ThreadsInChain {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
