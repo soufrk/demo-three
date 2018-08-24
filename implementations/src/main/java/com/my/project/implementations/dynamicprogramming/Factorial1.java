8e17bf86d297f3c7782f5642caafb600c5ed3beb
Souvik Goswami <soufrk@gmail.com>
2018-08-24 11:53:31 +0530

    changed formatting

--- implementations/src/main/java/com/my/project/implementations/dynamicprogramming/Factorial1.java
+++ implementations/src/main/java/com/my/project/implementations/dynamicprogramming/Factorial1.java
@@ -8,37 +8,38 @@ package com.my.project.implementations.dynamicprogramming;
  */
 public class Factorial1 {
 
-	public static void main(String[] args) {
-		int[] numbers = {0, 1, 2, 3, 4, 5};
-		//System.out.println(calculateByIterarion(numbers[3]));
-		System.out.println(calculateByRecursion(numbers[3]));
+    public static void main(String[] args) {
+	int[] numbers = { 0, 1, 2, 3, 4, 5 };
+	System.out.println(calculateByIterarion(numbers[3]));
+	System.out.println(calculateByRecursion(numbers[3]));
+    }
+
+    private static long calculateByIterarion(int number) {
+	long result = 1;
+	if (number < 0)
+	    throw new IllegalArgumentException("Number cannot be negative");
+	/*
+	 * if(number == 0) result = 1;
+	 */
+	if (number > 1) {
+	    for (int i = 1; i <= number; i++)
+		result *= i;
 	}
-	
-	private static long calculateByIterarion(int number){
-		long result = 1;
-		if(number < 0)
-			throw new IllegalArgumentException("Number cannot be negative");
-		/*if(number == 0)
-			result = 1;*/
-		if(number > 1){
-			for(int i=1; i<=number;i++)
-				result *= i;
-		}
-		return result;
+	return result;
+    }
+
+    private static long calculateByRecursion(int number) {
+	long result = 1;
+	if (number < 0)
+	    throw new IllegalArgumentException("Number cannot be negative");
+	if (number == 0)
+	    result = 1;
+	if (number == 1) {
+	    result = 1;
+	} else {
+	    result = number * calculateByRecursion(number - 1);
 	}
-	
-	private static long calculateByRecursion(int number){
-		long result = 1;
-		if(number < 0)
-			throw new IllegalArgumentException("Number cannot be negative");
-		if(number == 0)
-			result = 1;
-		if(number == 1){
-			result = 1;
-		} else{
-			result = number * calculateByRecursion(number-1);
-		}
-		return result;
-	}
+	return result;
+    }
 
 }
